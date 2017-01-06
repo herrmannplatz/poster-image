2073920d3b2b717acfa5302e7453176874f70c1e
herrmannplatz <rene@herrmannplatz.de>
2017-01-06 23:08:08 +0100

    add poster.js

---
+++ poster.js
@@ -0,0 +1,56 @@
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define([], factory)
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory()
+  } else {
+    root.poster = factory()
+  }
+}(this, function () {
+  'use strict'
+
+  var captureFrame = function (video) {
+    var canvas = document.createElement('canvas')
+    canvas.width = video.videoWidth
+    canvas.height = video.videoHeight
+    canvas.getContext('2d').drawImage(video, 0, 0, canvas.width, canvas.height)
+    return canvas.toDataURL()
+  };
+
+  var loadVideoSrc = function (videoSrc) {
+    return new Promise(function(resolve) {
+      var video = document.createElement('video')
+      video.src = videoSrc
+
+      video.addEventListener('loadedmetadata', function(event) {
+        video.currentTime = 1
+      });
+
+      video.addEventListener('seeked', function() {
+        resolve(video)
+      });
+
+      video.addEventListener('error', function() {
+        reject(error)
+      });
+    })
+  }
+
+  var api = function(file) {
+    if (!(file instanceof Blob)) {
+      throw new Error('Expected file to be of type Blob')
+    }
+
+    return Promise.resolve().then(function() {
+      return URL.createObjectURL(file)
+    }).then(function(videoSrc) {
+      return loadVideoSrc(videoSrc)
+    }).then(function(video) {
+      return captureFrame(video)
+    }).catch(function(error) {
+      throw new Error('Failed to capture poster frame: ' + error.message)
+    })
+  }
+
+  return api
+}));
